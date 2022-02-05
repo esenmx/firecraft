1e2c8cf8fd9e883fc415c1ac6c19eb064c250aae
softronaut <mathronaut@gmail.com>
2022-02-05 18:24:03 +0300

    helpers export fix

--- lib/firestorex.dart
+++ lib/firestorex.dart
@@ -1,10 +1,10 @@
 library firestorex;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firestorex/src/utils.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 part 'src/extensions.dart';
+part 'src/helpers.dart';
 part 'src/input_formatter.dart';
 part 'src/pagination_view.dart';
--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -1,3 +1,5 @@
+part of firestorex;
+
 /// https://firebase.google.com/docs/firestore/query-data/queries#query_limitations
 /// todo docs
 const int kFirestoreQueryEqualityLimit = 10;
