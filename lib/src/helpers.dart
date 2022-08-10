55d75101172c0aa1fd71799094712a5df152aa16
esenmx <mehmetesen@protonmail.com>
2022-08-10 16:06:21 +0300

    feat: dataMap helpers

---
+++ lib/src/helpers.dart
@@ -0,0 +1,13 @@
+part of firestorex;
+
+extension QuerySnapshotX<D> on QuerySnapshot<D> {
+  Map<String, D> asDataMap() {
+    return {for (var d in docs) d.id: d.data()};
+  }
+}
+
+extension IterableDocumentSnapshotX<D> on Iterable<DocumentSnapshot<D>> {
+  Map<String, D?> asDataMap() {
+    return {for (var d in this) d.id: d.data()};
+  }
+}
