e5b43c2d43258144f34da91fb13620019d0fadf1
esenmx <mathronaut@gmail.com>
2022-08-24 12:27:52 +0300

    nullable DateTime/Timestamp convert

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -11,3 +11,7 @@ extension IterableDocumentSnapshotX<D> on Iterable<DocumentSnapshot<D>> {
     return {for (var d in this) d.id: d.data()};
   }
 }
+
+extension DateTimeFirestoreX on DateTime? {
+  Timestamp? get toTimestamp => this == null ? null : Timestamp.fromDate(this!);
+}
